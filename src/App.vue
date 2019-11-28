6c365a48bd22b244f00b3039da426e84d858ac38
HYscript-01 <aqi_hot_01@hotmail.com>
2019-11-28 14:59:13 +0800

    Vue3_gshop

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <router-view />
+    <FooterGuide v-show="$route.meta.isShowFooter" />
+  </div>
+</template>
+<script>
+// 引入底部组件
+import FooterGuide from "./components/FooterGuide/FooterGuide.vue";
+// 引入接口测试
+import { reqAddress, reqCategorys, reqShopList } from "./api";
+export default {
+  name: "App",
+  // 注册组件
+  components: {
+    FooterGuide
+  },
+  async mounted() {
+    await reqAddress({
+      longitude: "116.36867",
+      latitude: "40.10038"
+    });
+    await reqCategorys()
+    await reqShopList({
+      longitude: '116.36867',
+      latitude: '40.10038'
+    })
+  }
+};
+</script>
+<style lang="stylus" rel="stylesheet/stylus"></style>
